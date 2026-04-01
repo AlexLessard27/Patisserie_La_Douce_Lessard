958338db2639bada3a0d798b2626879c5805ff87
Steven Ladouceur <2434777@cegepba.qc.ca>
2026-04-01 12:21:49 -0400

    Correction de nomenclature

--- Patisserie/ImputationPieceForm.Designer.cs
+++ Patisserie/ImputationPieceForm.Designer.cs
@@ -29,7 +29,7 @@
         private void InitializeComponent()
         {
             rechercherPieceGroupBox = new GroupBox();
-            rechercherPIeceButton = new Button();
+            rechercherIngredientButton = new Button();
             numeroPieceTextBox = new TextBox();
             pieceDesireDGV = new DataGridView();
             label2 = new Label();
@@ -53,58 +53,65 @@
             // 
             // rechercherPieceGroupBox
             // 
-            rechercherPieceGroupBox.Controls.Add(rechercherPIeceButton);
+            rechercherPieceGroupBox.Controls.Add(rechercherIngredientButton);
             rechercherPieceGroupBox.Controls.Add(numeroPieceTextBox);
             rechercherPieceGroupBox.Controls.Add(pieceDesireDGV);
             rechercherPieceGroupBox.Controls.Add(label2);
             rechercherPieceGroupBox.Controls.Add(numeroPieceLabel);
             rechercherPieceGroupBox.Font = new Font("Segoe UI", 12F);
-            rechercherPieceGroupBox.Location = new Point(12, 12);
+            rechercherPieceGroupBox.Location = new Point(14, 16);
+            rechercherPieceGroupBox.Margin = new Padding(3, 4, 3, 4);
             rechercherPieceGroupBox.Name = "rechercherPieceGroupBox";
-            rechercherPieceGroupBox.Size = new Size(776, 259);
+            rechercherPieceGroupBox.Padding = new Padding(3, 4, 3, 4);
+            rechercherPieceGroupBox.Size = new Size(887, 345);
             rechercherPieceGroupBox.TabIndex = 0;
             rechercherPieceGroupBox.TabStop = false;
             rechercherPieceGroupBox.Text = "Rechercher une pièce avec ou sans scanneur";
             // 
-            // rechercherPIeceButton
+            // rechercherIngredientButton
             // 
-            rechercherPIeceButton.Location = new Point(467, 25);
-            rechercherPIeceButton.Name = "rechercherPIeceButton";
-            rechercherPIeceButton.Size = new Size(151, 29);
-            rechercherPIeceButton.TabIndex = 6;
-            rechercherPIeceButton.Text = "Rechercher";
-            rechercherPIeceButton.UseVisualStyleBackColor = true;
+            rechercherIngredientButton.Location = new Point(534, 33);
+            rechercherIngredientButton.Margin = new Padding(3, 4, 3, 4);
+            rechercherIngredientButton.Name = "rechercherIngredientButton";
+            rechercherIngredientButton.Size = new Size(173, 39);
+            rechercherIngredientButton.TabIndex = 6;
+            rechercherIngredientButton.Text = "Rechercher";
+            rechercherIngredientButton.UseVisualStyleBackColor = true;
+            rechercherIngredientButton.Click += rechercherPieceButton_Click;
             // 
             // numeroPieceTextBox
             // 
-            numeroPieceTextBox.Location = new Point(160, 25);
+            numeroPieceTextBox.Location = new Point(183, 33);
+            numeroPieceTextBox.Margin = new Padding(3, 4, 3, 4);
             numeroPieceTextBox.Name = "numeroPieceTextBox";
-            numeroPieceTextBox.Size = new Size(286, 29);
+            numeroPieceTextBox.Size = new Size(326, 34);
             numeroPieceTextBox.TabIndex = 5;
             // 
             // pieceDesireDGV
             // 
             pieceDesireDGV.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            pieceDesireDGV.Location = new Point(18, 89);
+            pieceDesireDGV.Location = new Point(21, 119);
+            pieceDesireDGV.Margin = new Padding(3, 4, 3, 4);
             pieceDesireDGV.Name = "pieceDesireDGV";
-            pieceDesireDGV.Size = new Size(739, 150);
+            pieceDesireDGV.RowHeadersWidth = 51;
+            pieceDesireDGV.Size = new Size(845, 200);
             pieceDesireDGV.TabIndex = 4;
             // 
             // label2
             // 
             label2.AutoSize = true;
-            label2.Location = new Point(18, 62);
+            label2.Location = new Point(21, 83);
             label2.Name = "label2";
-            label2.Size = new Size(206, 21);
+            label2.Size = new Size(258, 28);
             label2.TabIndex = 3;
             label2.Text = "Sélectionner la pièce désirée";
             // 
             // numeroPieceLabel
             // 
             numeroPieceLabel.AutoSize = true;
-            numeroPieceLabel.Location = new Point(18, 25);
+            numeroPieceLabel.Location = new Point(21, 33);
             numeroPieceLabel.Name = "numeroPieceLabel";
-            numeroPieceLabel.Size = new Size(136, 21);
+            numeroPieceLabel.Size = new Size(171, 28);
             numeroPieceLabel.TabIndex = 2;
             numeroPieceLabel.Text = "Numéro de pièce :";
             // 
@@ -113,9 +120,11 @@
             commandeGroupBox.Controls.Add(choisirCommandeLabel);
             commandeGroupBox.Controls.Add(choisirCommandeComboBox);
             commandeGroupBox.Font = new Font("Segoe UI", 12F);
-            commandeGroupBox.Location = new Point(12, 277);
+            commandeGroupBox.Location = new Point(14, 369);
+            commandeGroupBox.Margin = new Padding(3, 4, 3, 4);
             commandeGroupBox.Name = "commandeGroupBox";
-            commandeGroupBox.Size = new Size(776, 92);
+            commandeGroupBox.Padding = new Padding(3, 4, 3, 4);
+            commandeGroupBox.Size = new Size(887, 123);
             commandeGroupBox.TabIndex = 1;
             commandeGroupBox.TabStop = false;
             commandeGroupBox.Text = "Commande";
@@ -123,18 +132,19 @@
             // choisirCommandeLabel
             // 
             choisirCommandeLabel.AutoSize = true;
-            choisirCommandeLabel.Location = new Point(6, 39);
+            choisirCommandeLabel.Location = new Point(7, 52);
             choisirCommandeLabel.Name = "choisirCommandeLabel";
-            choisirCommandeLabel.Size = new Size(178, 21);
+            choisirCommandeLabel.Size = new Size(221, 28);
             choisirCommandeLabel.TabIndex = 1;
             choisirCommandeLabel.Text = "Choisir une commande :";
             // 
             // choisirCommandeComboBox
             // 
             choisirCommandeComboBox.FormattingEnabled = true;
-            choisirCommandeComboBox.Location = new Point(214, 39);
+            choisirCommandeComboBox.Location = new Point(245, 52);
+            choisirCommandeComboBox.Margin = new Padding(3, 4, 3, 4);
             choisirCommandeComboBox.Name = "choisirCommandeComboBox";
-            choisirCommandeComboBox.Size = new Size(326, 29);
+            choisirCommandeComboBox.Size = new Size(372, 36);
             choisirCommandeComboBox.TabIndex = 0;
             // 
             // employeGroupBox
@@ -143,9 +153,11 @@
             employeGroupBox.Controls.Add(comboBox1);
             employeGroupBox.Controls.Add(choisirEmployeButton);
             employeGroupBox.Font = new Font("Segoe UI", 12F);
-            employeGroupBox.Location = new Point(12, 375);
+            employeGroupBox.Location = new Point(14, 500);
+            employeGroupBox.Margin = new Padding(3, 4, 3, 4);
             employeGroupBox.Name = "employeGroupBox";
-            employeGroupBox.Size = new Size(776, 83);
+            employeGroupBox.Padding = new Padding(3, 4, 3, 4);
+            employeGroupBox.Size = new Size(887, 111);
             employeGroupBox.TabIndex = 1;
             employeGroupBox.TabStop = false;
             employeGroupBox.Text = "Employé";
@@ -153,25 +165,27 @@
             // choisirEmployeLabel
             // 
             choisirEmployeLabel.AutoSize = true;
-            choisirEmployeLabel.Location = new Point(6, 33);
+            choisirEmployeLabel.Location = new Point(7, 44);
             choisirEmployeLabel.Name = "choisirEmployeLabel";
-            choisirEmployeLabel.Size = new Size(152, 21);
+            choisirEmployeLabel.Size = new Size(189, 28);
             choisirEmployeLabel.TabIndex = 2;
             choisirEmployeLabel.Text = "Choisir un employé :";
             // 
             // comboBox1
             // 
             comboBox1.FormattingEnabled = true;
-            comboBox1.Location = new Point(214, 28);
+            comboBox1.Location = new Point(245, 37);
+            comboBox1.Margin = new Padding(3, 4, 3, 4);
             comboBox1.Name = "comboBox1";
-            comboBox1.Size = new Size(326, 29);
+            comboBox1.Size = new Size(372, 36);
             comboBox1.TabIndex = 1;
             // 
             // choisirEmployeButton
             // 
-            choisirEmployeButton.Location = new Point(569, 28);
+            choisirEmployeButton.Location = new Point(650, 37);
+            choisirEmployeButton.Margin = new Padding(3, 4, 3, 4);
             choisirEmployeButton.Name = "choisirEmployeButton";
-            choisirEmployeButton.Size = new Size(188, 31);
+            choisirEmployeButton.Size = new Size(215, 41);
             choisirEmployeButton.TabIndex = 0;
             choisirEmployeButton.Text = "Choisir un employé";
             choisirEmployeButton.UseVisualStyleBackColor = true;
@@ -180,40 +194,43 @@
             // 
             quantiteLabel.AutoSize = true;
             quantiteLabel.Font = new Font("Segoe UI", 12F);
-            quantiteLabel.Location = new Point(18, 475);
+            quantiteLabel.Location = new Point(21, 633);
             quantiteLabel.Name = "quantiteLabel";
-            quantiteLabel.Size = new Size(77, 21);
+            quantiteLabel.Size = new Size(97, 28);
             quantiteLabel.TabIndex = 2;
             quantiteLabel.Text = "Quantité :";
             // 
             // quatiteNUD
             // 
-            quatiteNUD.Location = new Point(116, 478);
+            quatiteNUD.Location = new Point(133, 637);
+            quatiteNUD.Margin = new Padding(3, 4, 3, 4);
             quatiteNUD.Name = "quatiteNUD";
-            quatiteNUD.Size = new Size(120, 23);
+            quatiteNUD.Size = new Size(137, 27);
             quatiteNUD.TabIndex = 3;
             // 
             // button1
             // 
             button1.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            button1.Location = new Point(260, 475);
+            button1.Location = new Point(297, 633);
+            button1.Margin = new Padding(3, 4, 3, 4);
             button1.Name = "button1";
-            button1.Size = new Size(175, 35);
+            button1.Size = new Size(200, 47);
             button1.TabIndex = 4;
             button1.Text = "Ajouter imputation";
             button1.UseVisualStyleBackColor = true;
             // 
             // ImputationPieceForm
             // 
-            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 526);
+            ClientSize = new Size(914, 701);
             Controls.Add(button1);
             Controls.Add(quatiteNUD);
             Controls.Add(quantiteLabel);
             Controls.Add(employeGroupBox);
             Controls.Add(commandeGroupBox);
             Controls.Add(rechercherPieceGroupBox);
+            Margin = new Padding(3, 4, 3, 4);
             Name = "ImputationPieceForm";
             Text = "Faire une imputation";
             rechercherPieceGroupBox.ResumeLayout(false);
@@ -231,7 +248,7 @@
         #endregion
 
         private GroupBox rechercherPieceGroupBox;
-        private Button rechercherPIeceButton;
+        private Button rechercherIngredientButton;
         private TextBox numeroPieceTextBox;
         private DataGridView pieceDesireDGV;
         private Label label2;

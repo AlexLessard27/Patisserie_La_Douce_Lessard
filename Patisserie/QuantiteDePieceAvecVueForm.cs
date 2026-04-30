5b446182bdfab081cf49357bb26995aa6d47a0db
Alexandre Lessard <1438952@cegepba.qc.ca>
2026-04-30 11:43:59 -0400

    Complétion de la sauvegarde et rollback

--- Patisserie/QuantiteDePieceAvecVueForm.cs
+++ Patisserie/QuantiteDePieceAvecVueForm.cs
@@ -1,5 +1,6 @@
-﻿using Patisserie.Models;
-using System.Data;
+﻿using System.Data;
+using System.Windows.Input;
+using Patisserie.Models;
 
 namespace Patisserie
 {
@@ -84,14 +85,65 @@ namespace Patisserie
 
         private void Enregistrer(object? idCommande, object? idRecette, object? idIngredient, int nouvelleValeur)
         {
-            if (idCommande == null)
+            if (idCommande == null || idRecette ==  null || idIngredient == null)
             {
                 return;
             }
 
-            using (var context = new LaDouceLessardContext())
+            int cmdId = Convert.ToInt32(idCommande);
+            int recId = Convert.ToInt32(idRecette);
+
+            try
             {
-               
+                using (var context = new LaDouceLessardContext())
+                {
+                    var utilise = context.Utilises.FirstOrDefault(u => u.IdCommande == cmdId && u.IdRecette == recId);
+
+                    if (utilise == null)
+                    {
+                        return;
+                    }
+
+                    int ancienneValeur = utilise.QuantitePrevue;
+
+                    if (nouvelleValeur <= 0)
+                    {
+                        MessageBox.Show("La quantité prévue doit être supérieur à 0 !");
+
+                        RevenirAncienneValeur(cmdId, recId, idIngredient, ancienneValeur);
+
+                        return;
+                    }
+
+                    utilise.QuantitePrevue = nouvelleValeur;
+
+                    context.SaveChanges();
+                }
+            }
+            catch (Exception ex)
+            { 
+                MessageBox.Show("Erreur lors de l'enregistrement : " + ex.Message);
+
+                RevenirAncienneValeur(Convert.ToInt32(idCommande), Convert.ToInt32(idRecette), idIngredient, null);
+            }
+           
+        }
+
+        private void RevenirAncienneValeur(int idCommande, int idRecette, object idIngredient, int? ancienneValeur)
+        {
+            foreach (DataGridViewRow row in ingredientsVueDGV.Rows)
+            {
+                if (Convert.ToInt32(row.Cells["IdCommande"].Value) == idCommande &&
+                    Convert.ToInt32(row.Cells["IdRecette"].Value) == idRecette &&
+                    Convert.ToInt32(row.Cells["IdIngredient"].Value) == Convert.ToInt32(idIngredient))
+                {
+                    if (ancienneValeur != null)
+                    {
+                        row.Cells["QuantitePrevue"].Value = ancienneValeur;
+
+                        break;
+                    }
+                }
             }
         }
     }
